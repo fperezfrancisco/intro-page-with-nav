b682493f2bf6d0b87ad3c8b823544ae7f9e9178e
francisco <fperezfrancisco4@gmail.com>
2023-04-13 09:42:37 -0700

    finished mobile nav menu

--- script.js
+++ script.js
@@ -7,8 +7,34 @@ const featureArrowDown = document.getElementById("featureArrowDown");
 const featureArrowUp = document.getElementById("featureArrowUp");
 const companyArrowDown = document.getElementById("companyArrowDown");
 const companyArrowUp = document.getElementById("companyArrowUp");
+/** mobile menu sub drop down */
+const featureLiMobile = document.getElementById("mobileFeatureLi");
+const companyLiMobile = document.getElementById("mobileCompanyLi");
+const featuresDropDownMobile = document.querySelector(
+  ".featuresDropDownMobile"
+);
+const companyDropDownMobile = document.querySelector(".companyDropDownMobile");
+const featureArrowDownMobile = document.getElementById(
+  "featureArrowDownMobile"
+);
+const featureArrowUpMobile = document.getElementById("featureArrowUpMobile");
+const companyArrowDownMobile = document.getElementById(
+  "companyArrowDownMobile"
+);
+const companyArrowUpMobile = document.getElementById("companyArrowUpMobile");
 
 /* add event listeners */
+featureLiMobile.addEventListener("click", () => {
+  featureArrowDownMobile.classList.toggle("hidden");
+  featureArrowUpMobile.classList.toggle("hidden");
+  featuresDropDownMobile.classList.toggle("hidden");
+});
+
+companyLiMobile.addEventListener("click", () => {
+  companyArrowDownMobile.classList.toggle("hidden");
+  companyArrowUpMobile.classList.toggle("hidden");
+  companyDropDownMobile.classList.toggle("hidden");
+});
 
 featureLi.addEventListener("click", () => {
   featureArrowDown.classList.toggle("hidden");
@@ -21,3 +47,18 @@ companyLi.addEventListener("click", () => {
   companyArrowUp.classList.toggle("hidden");
   companyDropDown.classList.toggle("hidden");
 });
+
+/** mobile menu actions*/
+
+const mobileNav = document.querySelector(".mobileNav");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".mobileCloseBtn");
+const openBtn = document.querySelector(".menuOpen");
+
+function toggleMenu() {
+  mobileNav.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+closeBtn.addEventListener("click", toggleMenu);
+openBtn.addEventListener("click", toggleMenu);
